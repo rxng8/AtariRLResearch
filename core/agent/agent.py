78b4b2d1b6d781a2cf409fad163bfab7bfd6adce
Alex Nguyen <alexvn.edu@gmail.com>
2021-02-13 22:34:41 -0500

    full pipeline

--- core/agent/agent.py
+++ core/agent/agent.py
@@ -1,5 +1,7 @@
 
 
+from core.models.utils import mse
+from tensorflow.python.keras.engine import training
 from core import models
 import random
 import numpy as np
@@ -15,18 +17,44 @@ class BaseAgent:
         pass
 
 class DQNAgent(BaseAgent):
-    def __init__(self, model, action_size: int, eps_func) -> None:
+    def __init__(self, action_size: int, eps_func) -> None:
         super().__init__()
-        self.model = model
         self.eps_func = eps_func
         self.action_size = action_size
+        self.optimizer = tf.keras.optimizers.Adam()
 
-    def best_action(self, state) -> int:
-        return self.model([state])[0]
+    def best_action(self, model, state) -> int:
+        q_vector = model([state])[0]
+        return tf.argmax(q_vector)
 
-    def fit_batch(self, batch):
+    def fit_batch(self, model, batch, discount_rate: float=0.97):
+        (states, actions, rewards, new_states, terminals), \
+            importance, indices = batch
 
-        pass
+        with tf.device('/device:GPU:0'):
+            # Open a GradientTape to record the operations run
+            # during the forward pass, which enables auto-differentiation.
+            with tf.GradientTape() as tape:
+
+                pred_q_vec = model(states, training=True)  # Logits for this minibatch
+                pred_q_value = pred_q_vec[actions]
+
+                # Compute the loss value for this minibatch.
+                true_q_value = rewards
+                if not terminals:
+                    true_q_value += discount_rate * model(new_states, training=False)
+                loss = mse(true_q_value, pred_q_value)
+
+                # print(f"loss for this batch at step: {step + 1}: {loss }")
+
+            # Use the gradient tape to automatically retrieve
+            # the gradients of the trainable variables with respect to the loss.
+            grads = tape.gradient(loss, model.trainable_weights)
+
+            # Run one step of gradient descent by updating
+            # the value of the variables to minimize the loss.
+            self.optimizer.apply_gradients(zip(grads, model.trainable_weights))
+
     
     def q_step(self,
         env, # One agent can play in many env, env does not need to belong to an agent
@@ -52,10 +80,12 @@ class DQNAgent(BaseAgent):
         memory.add_experience(action, state, reward, terminal)
 
         # Create batch
+        # (A tuple of states, actions, rewards, new_states, and terminals),
+        # importance, indices.
         batch = memory.get_minibatch(batch_size)
 
         # Train batch
-        self.fit_batch(batch, model)
+        self.fit_batch(model, batch)
 
         return observation, terminal
 
